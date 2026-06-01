1e343c22062850b8057a56b1b8e1fcab3e4a787c
M Uzair <ukuzair708050@gmail.com>
2026-06-01 14:08:35 +0500

    Env-Error-Fix

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // This will be overridden by your production backend URL once deployed on DigitalOcean
+  apiUrl: 'https://your-digitalocean-app-url.ondigitalocean.app' 
+};
